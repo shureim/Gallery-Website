05d5517819e73824fc667815bce74b1ee09268de
shureim <tarickaliabdi@gmail.com>
2018-11-04 18:23:41 +0300

    added the models.py and also created the photographer class

--- photo/models.py
+++ photo/models.py
@@ -1,15 +1,15 @@
-# from django.db import models
-# import datetime as dt
-#
-# # Create your models here.
-#
-#
-#
-# class Photographer(models.Model):
-#     first_name = models.CharField(max_length =30)
-#     last_name = models.CharField(max_length =30)
-#     email = models.EmailField()
-#
+from django.db import models
+import datetime as dt
+
+# Create your models here.
+
+
+
+class Photographer(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+# 
 #     def __str__(self):
 #         return self.first_name
 #
