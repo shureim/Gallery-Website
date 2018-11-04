40c9976a92c79d25f87ee06605ef08f2fea028f7
shureim <tarickaliabdi@gmail.com>
2018-11-04 18:36:37 +0300

    added the  image  class

--- photo/models.py
+++ photo/models.py
@@ -46,63 +46,55 @@ class category(models.Model):
         self.delete()
 
     @classmethod
-    def update_location(cls,location,update):
-         updated = cls.objects.filter(image_name=location).update(name=update)
+    def update_category(cls,category,update):
+         updated = cls.objects.filter(image_name=category).update(name=update)
          return updated
-#
-#     verbose_name_plural = "Categories"
-#
-# class Image(models.Model):
-#     image = models.ImageField(upload_to = 'images/',blank = True)
-#     image_name = models.CharField(max_length = 30)
-#     image_description = models.TextField(max_length = 200)
-#     photography = models.ForeignKey(Photographer)
-#     image_category = models.ManyToManyField(category)
-#     image_location = models.ForeignKey(location)
-#     photo_date = models.DateTimeField(auto_now_add=True)
-#
-#     @classmethod
-#     def todays_photo(cls):
-#         today = dt.date.today()
-#         photo = cls.objects.filter(photo_date__date = today)
-#         return photo
-#
-#     @classmethod
-#     def days_photo(cls,date):
-#         photo = cls.objects.filter(photo_date = date)
-#         return photo
-#
-#
-#     @classmethod
-#     def get_all(cls):
-#         images = cls.objects.order_by('-post_date')
-#         return images
-#
-#     @classmethod
-#     def get_image(cls, id):
-#         image = cls.objects.get(id=id)
-#         return image
-#
-#     @classmethod
-#     def searched(cls, query):
-#         result = cls.objects.filter(image_description =query).order_by('-post_date')
-#         return result
-#
-#
-#
-#     # @classmethod
-#     # def today_pics(cls):
-#     #     today = dt.date.today()
-#     #     images = cls.objects.filter(post_date__date=today)
-#     #     return images
-#
-#
-#     @classmethod
-#     def filter_by_Location(cls, loc):
-#         loc = cls.objects.filter(location=loc)
-#         return cls.objects.filter(location=loc)
-#
-#     @classmethod
-#     def search_by_category(cls,search_term):
-#         photo = cls.objects.filter(image_category = search_term)
-#         return photo
+
+    verbose_name_plural = "Categories"
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/',blank = True)
+    image_name = models.CharField(max_length = 30)
+    image_description = models.TextField(max_length = 200)
+    photography = models.ForeignKey(Photographer)
+    image_category = models.ManyToManyField(category)
+    image_location = models.ForeignKey(location)
+    photo_date = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def todays_photo(cls):
+        today = dt.date.today()
+        photo = cls.objects.filter(photo_date__date = today)
+        return photo
+
+    @classmethod
+    def days_photo(cls,date):
+        photo = cls.objects.filter(photo_date = date)
+        return photo
+
+
+    @classmethod
+    def get_all(cls):
+        images = cls.objects.order_by('-post_date')
+        return images
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def searched(cls, query):
+        result = cls.objects.filter(image_description =query).order_by('-post_date')
+        return result
+
+
+    @classmethod
+    def filter_by_Location(cls, loc):
+        loc = cls.objects.filter(location=loc)
+        return cls.objects.filter(location=loc)
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        photo = cls.objects.filter(image_category = search_term)
+        return photo
