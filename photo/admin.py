8a3dc05750588fb50bff83e6646b46c5cccbdddf
shureim <tarickaliabdi@gmail.com>
2018-11-04 18:21:29 +0300

    imported Image class to admin

--- photo/admin.py
+++ photo/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Photographer
+from .models import Photographer,Image
 
 # Register your models here.
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal = ('location','category')
 admin.site.register(Photographer)
-# admin.site.register(Image)
+admin.site.register(Image)
 # admin.site.register(location)
 # admin.site.register(category)
